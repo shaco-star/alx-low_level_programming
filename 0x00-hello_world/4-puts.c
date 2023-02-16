66344c4cd72776b3154f1cf70ae8adcc20666096
shaco-star <sherifm846@gmail.com>
2023-02-16 00:10:42 -0800

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 
 /**
- * main - Entry point of the program
+ * main: Entry point of the program
  * 
  * Description: A C program that prints with put function
  * 
  * Return: Always 0 (Success)
  */
 
-
-
-
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
