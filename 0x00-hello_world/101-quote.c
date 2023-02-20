d31047c91bb8290b8315fada9db7bf056f3bf69b
shaco-star <sherifm846@gmail.com>
2023-02-19 22:25:28 -0800

    Extra

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
@@ -13,7 +13,7 @@ int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(1, q, 59);
+	write(1, q, 59);
 
 	return (1);
 
