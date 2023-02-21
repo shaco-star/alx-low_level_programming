ddd6f33eece6e0fbbbd5b54fce992a9adc194b16
shaco-star <sherifm846@gmail.com>
2023-02-21 02:24:55 -0800

    init

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 		for (k = 0; k <= 9; k++)
 		{
 			multi = i * k;
-			if (!(multi / 10))
+			if ((multi / 10))
 				_putchar((multi / 10) + '0');
 			_putchar((multi %10) + '0');
 			_putchar(',');
