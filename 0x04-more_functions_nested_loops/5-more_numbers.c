576d157df7e40563c5cd6f36be4e255171ff4e6d
shaco-star <sherifm846@gmail.com>
2023-02-23 02:29:09 -0800

    init

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,7 +6,7 @@
  * Return: 0 success
 */
 
-void print_numbers(void)
+void more_numbers(void)
 {
 	int i, k, num;
 
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,11 @@ int print_line(int n)
 {
 	int i;
 
-	if (n !=0 && n > 0)
+	if (n != 0 && n > 0)
 	{
 		for (i = 0; i < n; i++)
 			_putchar('_');
-	
+
 	}
 	_putchar('\n');
 }
