b5fc948518434099e983eef4a36e496216603b2c
shaco-star <sherifm846@gmail.com>
2023-02-23 04:49:03 -0800

    init

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers from 1 to 100 if
+ * number multiples by 3 print Fizz, if by 5 print Buzz
+ * if both print FizzBuzz
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fiz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		putchar(' ');
+
+	}
+	putchar('\n');
+}
