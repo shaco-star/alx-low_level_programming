cdb9cd5876d184539a11d71cf871c25b32703779
shaco-star <sherifm846@gmail.com>
2023-02-23 03:24:32 -0800

    init

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,7 @@ void print_diagonal(int n)
 
 	if (n != 0 && n > 0)
 	{
-		for (space = 0; space < i; space++)
+		for (space = 0; space <= i; space++)
 			_putchar(' ');
 
 		for (i = 0; i < n; i++)
