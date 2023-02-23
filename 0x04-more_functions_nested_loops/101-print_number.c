aba1ed43b8c729c358b941bd78e3193b0801f96b
shaco-star <sherifm846@gmail.com>
2023-02-23 11:01:09 -0800

    init

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_number - Print numbers
+ *
+ * @n: the number we want to print
+ *
+ * Return: 0 success
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if ((n / 10) > 0)
+		print_number(n / 10);
+
+	_putchar((n % 10) + 48);
+
+}
