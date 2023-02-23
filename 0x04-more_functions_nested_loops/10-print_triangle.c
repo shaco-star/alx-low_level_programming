fe7cdb572914ea86d81070d0784a6234237ea09d
shaco-star <sherifm846@gmail.com>
2023-02-23 06:08:41 -0800

    init

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include"main.c"
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: number of triangle
+ *
+ * Return: 0 success
+*/
+
+void print_triangle(int size)
+{
+	int row, space, i = size, hash;
+
+	for (row = 0; row < size; row++)
+	{
+		--i;
+		hash = size - i;
+
+		for (space = 0; space < i; space++)
+		{
+			_putchar(' ');
+		}
+		for (int k = 0; k < hash; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+
+	}
+}
