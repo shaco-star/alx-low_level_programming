d32daa4e990261aea0e252da184748ac06f2a791
shaco-star <sherifm846@gmail.com>
2023-02-27 00:15:37 -0800

    init

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include"main.c"
+
+/**
+ * print_rev - print the string in rev
+ *
+ * @s: string
+*/
+
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; len++)
+		;
+	for (len -= 1; len >= 0; len--)
+		_putchar(s[len]);
+	_putchar('\n');
+}
