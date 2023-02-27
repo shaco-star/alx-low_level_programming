98cccd035914c5bca00c516812baae825f9453b9
shaco-star <sherifm846@gmail.com>
2023-02-26 23:15:51 -0800

    init

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _puts - print string
+ *
+ * @str: string input
+*/
+
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
