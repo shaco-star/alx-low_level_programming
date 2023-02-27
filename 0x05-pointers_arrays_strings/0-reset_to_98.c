804e58422e93693bdfb3cdf71e019afe9c64998d
shaco-star <sherifm846@gmail.com>
2023-02-26 22:49:46 -0800

    init

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * reset_to_98 - update value of number to 98 using pointer
+ *
+ * @n: pointer input
+ *
+ * Return: 0 success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
