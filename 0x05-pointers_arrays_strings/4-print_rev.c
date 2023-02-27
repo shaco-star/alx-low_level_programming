646765827a22fae9a40b0a94f33f5675c527dc65
shaco-star <sherifm846@gmail.com>
2023-02-27 00:24:40 -0800

    init

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-#include"main.c"
+#include"main.h"
 
 /**
  * print_rev - print the string in rev
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * rev_string - print the string in rev
+ *
+ * @s: string
+*/
+
+void rev_string(char *s)
+{
+	int len, index;
+	char *start, *end, temp;
+
+	for (len = 0; *s != '\0'; len++)
+		;
+
+	for (index = 0; index < len - 1; index++)
+		end++;
+	for (index = 0; len / 2; index++)
+	{
+		temp = *end;
+		*end = *start;
+		*start = temp;
+	}
+	_putchar('\n');
+}
