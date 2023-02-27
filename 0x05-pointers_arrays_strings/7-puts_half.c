53c9c83399b6bccb930db61408601b63f0b8c665
shaco-star <sherifm846@gmail.com>
2023-02-27 01:03:22 -0800

    init

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include"main.h"
+
+/**
+ * puts_half - function that prints half of a string
+ *
+ * @str: string
+*/
+
+void puts_half(char *str)
+{
+	int len, i;
+
+
+	for (len = 0; str[len] != '\0'; len)
+		;
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for  (i = ((len - 1) / 2) + 1; str[i] != '\0'; i++)
+			_putchar(str[i]);
+
+	}
+
+	_putchar('\n');
+}
