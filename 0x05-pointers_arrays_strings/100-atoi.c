61f2bd19524a378c19fa41afd1cf0451e6711810
shaco-star <sherifm846@gmail.com>
2023-02-28 10:52:27 -0800

    init

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * _atoi - convert string to integer
+ *
+ * @s: string
+ *
+ * Return: integer after convertion
+*/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (sign * num);
+}
