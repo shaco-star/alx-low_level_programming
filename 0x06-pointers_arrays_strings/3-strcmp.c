e2fbb5c875dffd561bf68aa13eace7edc7c50528
shaco-star <sherifm846@gmail.com>
2023-03-01 11:37:24 -0800

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	
+
 	while (*s1 && *s1 == *s2)
 	{
 		++s1;
 		++s2;
 	}
 
-	return (int)(unsigned char)(*s1) - (int)(unsigned char)(*s2);
+	return ((int)(unsigned char)(*s1) - (int)(unsigned char)(*s2));
 
 
 
