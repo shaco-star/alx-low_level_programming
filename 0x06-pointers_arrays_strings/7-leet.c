8341d6377c3a811339cc76fe544ea3f037f2f627
shaco-star <sherifm846@gmail.com>
2023-03-01 21:54:49 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * leet - encode a string into 1337
+ *
+ * @s: string
+ *
+ * Return: encoded string
+*/
+
+char *leet(char s)
+{
+	int c, i = 0;
+	int upper[] = {97, 101, 111, 116, 108};
+	int lower[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
+
+	while (s[c] !='\0')
+	{
+		for (i = 0; i <= 4; i++)
+		{
+			if (s[c] == upper[i] || s[c] == lower[i])
+				s[c] = n[i];
+		}
+		c++;
+	}
+	return (s);
+}
