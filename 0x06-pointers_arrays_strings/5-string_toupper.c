39a9e5595d4fdca488eb1650db2e9744b9f6d8ab
shaco-star <sherifm846@gmail.com>
2023-03-01 21:30:07 -0800

    commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,14 @@
 
 char *string_toupper(char *str)
 {
-	while (*str)
+	int i = 0;
+
+
+	while (str[i] != '\0')
 	{
-		if (*str >= 97 && *str <= 122)
-			*str -= 32;
-		++str;
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+		++i;
 	}
 
 	return (str);
