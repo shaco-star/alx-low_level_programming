560f4c4b2f3150634e01611644b1d8023c2df478
shaco-star <sherifm846@gmail.com>
2023-03-08 10:59:44 -0800

    commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include"main.h"
+
+/**
+ * s_prime_number - checks if a number is prime or not recursively
+ * @n: input integer
+ *
+ * Return: 1 if n is prime, otherwise 0.
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+
+	return (is_prime_helper(n, 2));
+}
+
+/**
+ * is_prime_helper - helper function that does the recursive computation
+ * @n: input integer
+ * @i: counter variable to check divisibility of n
+ *
+ * Return: 1 if n is prime, otherwise 0.
+*/
+
+int is_prime_helper(int n, int i)
+{
+	if (i > n / 2)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (is_prime_helper(n, i + 1));
+}
+
