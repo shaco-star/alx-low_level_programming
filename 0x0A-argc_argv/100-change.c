cea2c6e7aacf183c4e87780b8713f236490d7e6f
shaco-star <sherifm846@gmail.com>
2023-03-10 11:16:43 -0800

    advanced

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+/**
+ * main - main entry point for the program
+ *
+ * @argc: number of items entered in the command line
+ * @argv: array of strings for entered commands
+ *
+ * Return: 0 Success
+*/
+
+int main(int argc, char *argv[])
+{
+	int coin, cent;
+
+	coin = 0;
+	if (argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cent = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	coins += cents / 25;
+	cents = cents % 25;
+
+	coins += cents / 10;
+	cents = cents % 10;
+
+	coins += cents / 5;
+	cents = cents % 5;
+
+	coins += cents / 2;
+	cents = cents % 2;
+
+	coins += cents;
+
+	printf("%d\n", coins);
+	return (0);
+}
+
+
