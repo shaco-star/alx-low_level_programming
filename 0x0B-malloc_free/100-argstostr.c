b620951e0caf30c84440d9c8f713b9e10b8eaf7c
shaco-star <sherifm846@gmail.com>
2023-03-14 13:20:19 -0700

    commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+int _strlen(char *s);
+
+/**
+ * argstostr - concatenates all arguments
+ *
+ * @ac: lenght of arguments
+ * @av: array of arguments
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j ,k = 0 ,len = 0;
+	char *arr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		len += _strlen(av[i]) + 1;
+
+
+	arr = malloc(sizeof(char) * len);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			arr[k] = av[i][j];
+		arr[k] = '\n';
+		k++;
+	}
+
+	arr[k] = '\0';
+
+	return (arr);
+
+}
+
+/**
+ * _strlen - calculate lenght of a string
+ *
+ * @s: string
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+
+	return (len);
+}
+
