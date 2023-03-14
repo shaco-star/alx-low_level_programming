34486eadd007b23860ed1f2e16d8194bd1f18e27
shaco-star <sherifm846@gmail.com>
2023-03-13 22:49:45 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s2[i] != '\0'; i++)
 		len2++;
 
-	s = (char *)malloc(sizeof(char) * (len1 + len2 - 3));
+	s = (char *)malloc(sizeof(char) * (len1 + len2));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
