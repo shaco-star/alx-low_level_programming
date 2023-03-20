9fc3e5625b405aaa76828da2bfea9dc5b038e5a2
shaco-star <sherifm846@gmail.com>
2023-03-20 05:01:19 -0700

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - dog struct
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
