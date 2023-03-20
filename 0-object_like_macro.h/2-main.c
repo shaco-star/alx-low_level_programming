15c9414ccb4578eebb722550032f60ed247d7ac0
shaco-star <sherifm846@gmail.com>
2023-03-20 02:08:36 -0700

    commit

---
+++ 0-object_like_macro.h/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print name of file that was compiled
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+
+	return (0);
+}
---
+++ 0-object_like_macro.h/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _ABS_FUNCTION
+#define _ABS_FUNCTION
+
+#define ABS(x) (((x) < 0) ? ((x) * -1) : (x))
+
+#endif /* _ABS_FUNCTION */
