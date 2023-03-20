b262a52d0cb456ae1c14a08c3830ce6c7bf2fbaa
shaco-star <sherifm846@gmail.com>
2023-03-20 05:09:39 -0700

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * init_dog - initialize struct
+ *
+ * @d: dog struct
+ * @name: string name of dog
+ * @age: float age of dog
+ * @owner: name of owner of dog
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
