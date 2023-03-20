27118aa4ae69edf857a2aa518cc958d6ff06e3dc
shaco-star <sherifm846@gmail.com>
2023-03-20 09:39:18 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG
 #define DOG
+
 /**
  * struct dog - dog struct
  *
@@ -14,14 +15,14 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- * dog_t - typedef for dog struct
-*/
 
-typedef struct dog dog_t;
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
