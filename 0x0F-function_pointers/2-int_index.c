c8f424f85711ac6b08816b9bd320afb39f73e316
shaco-star <sherifm846@gmail.com>
2023-03-22 07:10:47 -0700

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,13 +15,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL && size > 0 && cmp != NULL)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
 
 	return (-1);
