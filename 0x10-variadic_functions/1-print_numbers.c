a3fe29b8094108cd7d1b8da95cbf49ec5eb4bcad
shaco-star <sherifm846@gmail.com>
2023-03-23 05:38:37 -0700

    commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,8 +18,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != 0)
-			printf(" ");
 		printf("%d", va_arg(numPtr, int));
 		if (i != n - 1)
 			printf("%s", separator);
