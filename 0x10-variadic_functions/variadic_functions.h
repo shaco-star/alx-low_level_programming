2fa6eb44019e7e481ecb4f9bb48219be7cb321b7
shaco-star <sherifm846@gmail.com>
2023-03-23 14:12:57 -0700

    commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
