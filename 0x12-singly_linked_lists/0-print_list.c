4401ab69bf2ab964302cb92083f88ca52a594b97
shaco-star <sherifm846@gmail.com>
2023-03-30 08:25:39 -0700

    commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - print elements of a list
+ *
+ * @h: pointer to list
+ *
+ * Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	int len = 1;
+
+	while (h->next != NULL)
+	{
+
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		len++;
+
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	return (len);
+}
+
