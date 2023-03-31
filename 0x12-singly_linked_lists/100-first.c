b80032cabd9239b6e2a031e0b17bd00acbff57f8
shaco-star <sherifm846@gmail.com>
2023-03-31 16:19:58 -0700

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * premain - fun run before main
 */
 
-void __attribute__ ((constructor)) permain()
+void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
