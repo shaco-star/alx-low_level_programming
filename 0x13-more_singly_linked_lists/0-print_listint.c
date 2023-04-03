7cf9e53b65cfeaa8c05be082241e295d47fa5ed8
shaco-star <sherifm846@gmail.com>
2023-04-03 15:10:06 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all elements of list
+ *
+ * @h: list
+ *
+ * Return: number of elements or 0 if fails
+*/
+
+
+size_t print_listint(const listint_t* h) 
+{
+	size_t nodeCount = 1;
+
+	if (h == NULL)
+		return (0);
+	while (h->next != NULL) 
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodeCount++;
+	}
+	printf("%d\n", h->n);
+	return (nodeCount);
+}
