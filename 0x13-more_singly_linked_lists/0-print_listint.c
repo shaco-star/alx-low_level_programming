0b5aa3c08f172fcd787f039f1ac97cf8d65c771c
shaco-star <sherifm846@gmail.com>
2023-04-03 15:10:51 -0700

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 */
 
 
-size_t print_listint(const listint_t* h) 
+size_t print_listint(const listint_t *h)
 {
 	size_t nodeCount = 1;
 
 	if (h == NULL)
 		return (0);
-	while (h->next != NULL) 
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
