f5e3cc45d5b2753dde9995da7ab7ba39bf88d042
shaco-star <sherifm846@gmail.com>
2023-04-04 05:37:33 -0700

    commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+
 	for (i = 0; i < index && head != NULL; i++)
 	{
 		head = head->next;
