3b2aaace88878492689fbac0a206cf1dc965eec6
shaco-star <sherifm846@gmail.com>
2023-04-04 05:16:41 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free memory of node and set head to NULL
+ *
+ * @head: pointer to list
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+
+	while ((current = head) != NULL)
+	{
+
+		head = head->next;
+		free(current);
+
+	}
+	*head == NULL;
+}
+
