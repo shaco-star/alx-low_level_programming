e21d9df2e2fee3461d4551800c31ffe09b7acc23
shaco-star <sherifm846@gmail.com>
2023-04-04 06:11:47 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_listint - return nth node of linked list
+ *
+ * @head: head of node
+ * @index: index of node we want
+ *
+ * Return: return node at @index
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
