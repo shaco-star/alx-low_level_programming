3672ea31aa8a77ac7fc3b8660bd5e6bc187683ea
shaco-star <sherifm846@gmail.com>
2023-04-05 15:49:56 -0700

    commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,22 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp, *current;
 
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		current = *head;
 
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp);
+
+		}
 	}
+
 	*head = NULL;
 }
 
