dcbdd28d13ec3b3e04dd049057e274acc00c5295
shaco-star <sherifm846@gmail.com>
2023-04-05 15:57:53 -0700

    commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	for (i = 0; i <= index && *head != NULL; i++)
+	for (i = 0; i < index && *head != NULL; i++)
 	{
 		prev = temp;
 		temp = temp->next;
