dab5e755aec38bf173fa0130e6be54b8bdcd4cd8
shaco-star <sherifm846@gmail.com>
2023-04-08 17:19:21 -0700

    commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - get bit at @index
+ *
+ * @n: decimal number
+ * @index: index of bit that we will get
+ *
+ * Return: bit at @index or -1 if fail
+*/
+
+void get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (1);
+	return ((n >> index) & 1);
+}
