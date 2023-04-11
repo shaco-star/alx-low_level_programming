4f3d66dc5c249ddb77a7d68ec9ae2c5af38e5143
shaco-star <sherifm846@gmail.com>
2023-04-10 18:15:34 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp == -1)
 		return (0);
 
 	if (text_content)
 	{
-		while(text_content[word_len])
+		while (text_content[word_len])
 			word_len++;
 		n_write = write(fp, text_content, word_len);
 		if (n_write == -1)
