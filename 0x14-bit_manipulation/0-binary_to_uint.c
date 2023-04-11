784d5f19ef1536beea30492d5047040068464f5d
shaco-star <sherifm846@gmail.com>
2023-04-10 20:00:40 -0700

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,9 +1,5 @@
 #include"main.h"
 
-
-
-
-
 /**
  * binary_to_uint - convert string of binary to decimal
  *
@@ -18,13 +14,12 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-        while (*b)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
 		num = num * 2 + (*b++ - '0');
 	}
 	return (num);
-
 }
 
