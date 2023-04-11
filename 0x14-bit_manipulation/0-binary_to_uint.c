f4ccdee6d4bacc357de95bc8c9d23851693d8502
shaco-star <sherifm846@gmail.com>
2023-04-10 19:59:35 -0700

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,8 @@
 #include"main.h"
 
-/**
- * str_to_int - convert string to int
- *
- * @str: string
- *
- * Return: number or 0 if str is not 0 or 1
-*/
 
 
 
-unsigned int str_to_int(const char *str)
-{
-	int result = 0, i;
-
-	if (str == NULL)
-		return (0);
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] - '0' != 0 && str[i] - '0' != 1)
-			return (0);
-		result = result * 10 + str[i] - '0';
-	}
-	return (result);
-}
 
 /**
  * binary_to_uint - convert string of binary to decimal
@@ -36,18 +14,17 @@ unsigned int str_to_int(const char *str)
 
 unsigned int binary_to_uint(const char *b)
 {
-	int dec = 0, i = 0, rem, n = str_to_int(b);
+	unsigned int num = 0;
 
-	if (n == 0)
+	if (!b)
 		return (0);
-	while (n != 0)
+        while (*b)
 	{
-		rem = n % 10;
-		n /= 10;
-		dec += rem * (1 << i);
-		++i;
+		if (*b != '0' && *b != '1')
+			return (0);
+		num = num * 2 + (*b++ - '0');
 	}
-	return (dec);
+	return (num);
 
 }
 
