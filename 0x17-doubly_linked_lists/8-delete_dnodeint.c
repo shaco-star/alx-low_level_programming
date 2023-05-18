7107f04c9a817263476a1f3795a75056efffc8df
shaco-star <sherifm846@gmail.com>
2023-05-18 17:03:09 -0400

    update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,5 +11,26 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
 
+	if (*head == NULL)
+		return (-1);
+	while (temp != NULL && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp != NULL)
+	{
+		if (temp->prev != NULL)
+			temp->prev->next = temp->next;
+		else
+			*head = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+		free(temp);
+		return (1);
+	}
+	return (-1);
 }
