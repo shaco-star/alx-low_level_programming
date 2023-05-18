d375e27107eb50ef1e04bde2aac971537cdefe39
shaco-star <sherifm846@gmail.com>
2023-05-18 16:59:51 -0400

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,10 +12,34 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *head;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i = 0;
 
 	if (h == NULL)
 		return (NULL);
-	new_node = malloc
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (temp != NULL && i < idx)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL && i == idx)
+		return (add_dnodeint_end(h, n));
+	if (temp != NULL)
+	{
+		new_node->prev = temp->prev;
+		new_node->next = temp;
+		temp->prev->next = new_node;
+		temp->prev = new_node;
+		return (new_node);
+	}
+	free(new_node);
+	return (NULL);
 }
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete node at index
+ *
+ * @head: head of linked list
+ * @index: index that we want to delete node at it
+ *
+ * Return: 1 if secceeded, -1 if it failed
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+}
