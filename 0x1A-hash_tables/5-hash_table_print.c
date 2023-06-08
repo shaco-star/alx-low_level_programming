03d04656141e5d86d291c37434818621c30fc764
shaco-star <sherifm846@gmail.com>
2023-06-08 15:05:15 -0400

    commit

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,3 +1,27 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_print - prints hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	char *sep = "";
+	
+	if (!ht)
+		return (NULL);
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			printf("%s'%s': '%s'", sep, temp->key, temp->value);
+			sep = ", ";
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
