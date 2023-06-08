9493ab5c26cd12f7db14f3b139341f1ad1c37381
shaco-star <sherifm846@gmail.com>
2023-06-08 15:14:38 -0400

    commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,3 +1,29 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	hash_node_t *next;
 
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			next = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
