c6887f94457f5339d87dc0a965989fae4f9df4e2
shaco-star <sherifm846@gmail.com>
2023-07-26 14:24:00 -0400

    commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # Check top
+                if row == 0 or grid[row-1][col] == 0:
+                    perimeter += 1
+                # Check bottom
+                if row == rows-1 or grid[row+1][col] == 0:
+                    perimeter += 1
+                # Check left
+                if col == 0 or grid[row][col-1] == 0:
+                    perimeter += 1
+                # Check right
+                if col == cols-1 or grid[row][col+1] == 0:
+                    perimeter += 1
+
+    return perimeter
