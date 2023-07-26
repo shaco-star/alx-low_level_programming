3a8d6f200a74a539e645ce2bf91ef251a246501d
shaco-star <sherifm846@gmail.com>
2023-07-26 14:25:21 -0400

    commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+"""Define island_perimeter function"""
+
+
 def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid.
@@ -10,16 +14,12 @@ def island_perimeter(grid):
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                # Check top
                 if row == 0 or grid[row-1][col] == 0:
                     perimeter += 1
-                # Check bottom
                 if row == rows-1 or grid[row+1][col] == 0:
                     perimeter += 1
-                # Check left
                 if col == 0 or grid[row][col-1] == 0:
                     perimeter += 1
-                # Check right
                 if col == cols-1 or grid[row][col+1] == 0:
                     perimeter += 1
 
