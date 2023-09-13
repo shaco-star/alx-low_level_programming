939e0f6d6a408761bddc2e234ec70d16adf84466
shaco-star <sherifm846@gmail.com>
2023-09-13 14:55:41 -0400

    fixed-betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,14 @@
 #include "search_algos.h"
 
-
-
-
+/**
+ * linear_search - search for value in array using linear search
+ *
+ * @array: pointer to array
+ * @size: size of the array
+ * @value: value that we are searching for
+ *
+ * Return: index of value if found, -1 otherwise
+*/
 
 int linear_search(int *array, size_t size, int value)
 {
@@ -13,6 +19,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,54 @@
 #include "search_algos.h"
 
+/**
+ * print_array - print array.
+ *
+ * @array: array that we will print
+ * @start: start index of array
+ * @end: end index of array
+ *
+ * Return: none
+*/
 
-void print_array(int* array, size_t start, size_t end) {
-    printf("Searching in array: ");
-    for (size_t i = start; i < end; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
+void print_array(int *array, size_t start, size_t end)
+{
+	printf("Searching in array: ");
+	for (size_t i = start; i < end; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
 }
 
+/**
+ * binary_search - search array using binary search algo
+ *
+ * @array: pointer to array
+ * @size: size of the array
+ * @value: value that we are searching for
+ *
+ * Return: return index of searched value, -1 otherwise
+*/
 
-int binary_search(int* array, size_t size, int value) {
+int binary_search(int *array, size_t size, int value)
+{
+	size_t start = 0;
+	size_t end = size;
+	size_t mid;
 
-    size_t start = 0;
-    size_t end = size;
+	if (array == NULL)
+		return  (-1);
+	while (start < end)
+	{
+		print_array(array, start, end);
+		mid = start + (end - start) / 2;
 
-    if (array == NULL) {
-        return  (-1);
-    }
-    while (start < end) {
-        print_array(array, start, end);
-
-        size_t mid = start + (end - start) / 2;
-
-        if (array[mid] == value) {
-            return (mid);
-        }
-        else if (array[mid] < value) {
-            start = mid + 1;
-        }
-        else {
-            end = mid;
-        }
-    }
-
-    return (-1);
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			start = mid + 1;
+		else
+			end = mid;
+	}
+	return (-1);
 }
